9c1f36b8bb9539cf5d1e10305c7ff89a811e8e7a
MinPan9585 <94884915+MinPan9585@users.noreply.github.com>
2024-10-06 21:02:21 +0800

    game loop between battle and rock

--- Assets/Scripts/TwoPlayerRockPaperScissors.cs
+++ Assets/Scripts/TwoPlayerRockPaperScissors.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System;
+using System.Collections;
 
 public class TwoPlayerRockPaperScissors : MonoBehaviour
 {
@@ -20,13 +22,19 @@ public class TwoPlayerRockPaperScissors : MonoBehaviour
     public GameObject crabOnes;
     public GameObject crabTwos;
 
-    public Transform playerOneStartTrans;
-    public Transform playerTwoStartTrans;
+    Vector3 playerOneStartPos;
+    Vector3 playerTwoStartPos;
 
-
+    public GameObject guessPart;
+    public SpriteRenderer[] chara;
+    public Sprite[] charaImages;
 
     //pOne.currentHealth
-
+    private void Start()
+    {
+        playerOneStartPos = crabOne.transform.position;
+        playerTwoStartPos = crabTwo.transform.position;
+    }
     public void RockStart()
     {
         //crabOne.SetActive(false);
@@ -37,25 +45,65 @@ public class TwoPlayerRockPaperScissors : MonoBehaviour
         crabTwo.transform.GetChild(0).GetComponent<SpriteRenderer>().enabled = false;
         crabOnes.SetActive(true);
         crabTwos.SetActive(true);
+
+        guessPart.SetActive(true);
+        StartCoroutine("BlinkOne");
+        StartCoroutine("BlinkTwo");
+    }
+
+    public void AfterPressKeyOne()
+    {
+        StopCoroutine("BlinkOne");
+        for (int i = 0; i < 3; i++)
+        {
+            chara[i].sprite = charaImages[i * 2];
+        }
+    }
+    public void AfterPressKeyTwo()
+    {
+        StopCoroutine("BlinkTwo");
+        for (int i = 3; i < 6; i++)
+        {
+            chara[i].sprite = charaImages[i * 2];
+        }
     }
 
     public void Update()
     {
         //if ((pOneHealth.currentHealth == 0) || (pTwoHealth.currentHealth == 0))
         if (crabOnes.activeSelf || crabTwos.activeSelf)
-        {// Player 1 Input
-            if (Input.GetKeyDown(KeyCode.Q)) { SetPlayerChoice(1, Choice.Rock); }
-            if (Input.GetKeyDown(KeyCode.W)) { SetPlayerChoice(1, Choice.Paper); }
-            if (Input.GetKeyDown(KeyCode.E)) { SetPlayerChoice(1, Choice.Scissors); }
+        {
+            // Player 1 Input
+            if (Input.GetKeyDown(KeyCode.Q)) { SetPlayerChoice(1, Choice.Rock);
+                AfterPressKeyOne();
+            }
+            if (Input.GetKeyDown(KeyCode.W)) { SetPlayerChoice(1, Choice.Paper);
+                AfterPressKeyOne();
+            }
+            if (Input.GetKeyDown(KeyCode.E)) { SetPlayerChoice(1, Choice.Scissors);
+                AfterPressKeyOne();
+            }
 
             // Player 2 Input
-            if (Input.GetKeyDown(KeyCode.U)) { SetPlayerChoice(2, Choice.Rock); }
-            if (Input.GetKeyDown(KeyCode.I)) { SetPlayerChoice(2, Choice.Paper); }
-            if (Input.GetKeyDown(KeyCode.O)) { SetPlayerChoice(2, Choice.Scissors); }
+            if (Input.GetKeyDown(KeyCode.U)) { SetPlayerChoice(2, Choice.Rock);
+                AfterPressKeyTwo();
+            }
+            if (Input.GetKeyDown(KeyCode.I)) { SetPlayerChoice(2, Choice.Paper);
+                AfterPressKeyTwo();
+            }
+            if (Input.GetKeyDown(KeyCode.O)) { SetPlayerChoice(2, Choice.Scissors);
+                AfterPressKeyTwo();
+            }
 
             // Check if both players are ready
             if (player1Ready && player2Ready)
             {
+                if(player1Choice == Choice.Rock) { chara[0].sprite = charaImages[1]; }
+                if(player1Choice == Choice.Paper) { chara[1].sprite = charaImages[3]; }
+                if (player1Choice == Choice.Scissors) { chara[2].sprite = charaImages[5]; }
+                if (player2Choice == Choice.Rock) { chara[3].sprite = charaImages[7]; }
+                if (player2Choice == Choice.Paper) { chara[4].sprite = charaImages[9]; }
+                if (player2Choice == Choice.Scissors) { chara[5].sprite = charaImages[11]; }
                 DetermineWinner();
                 ResetChoices();
             }
@@ -99,17 +147,63 @@ public class TwoPlayerRockPaperScissors : MonoBehaviour
                 winner = "Player 2 wins!";
             }
             resultText.text = $"Player 1 chose: {player1Choice}\nPlayer 2 chose: {player2Choice}\n{winner}";
-            pOneHealth.currentHealth = pOneHealth.maxHealth;
-            pTwoHealth.currentHealth = pTwoHealth.maxHealth;
-            crabOne.transform.position = playerOneStartTrans.position;
-            crabTwo.transform.position = playerTwoStartTrans.position;
         }
-
+        //play animation
+        StartCoroutine("ResetToBattle");
     }
 
     private void ResetChoices()
     {
         player1Ready = false;
         player2Ready = false;
+    }
+
+    IEnumerator ResetToBattle()
+    {
+        yield return new WaitForSeconds(2);
+        crabOne.GetComponent<SpriteRenderer>().enabled = true;
+        crabOne.transform.GetChild(0).GetComponent<SpriteRenderer>().enabled = true;
+        crabTwo.GetComponent<SpriteRenderer>().enabled = true;
+        crabTwo.transform.GetChild(0).GetComponent<SpriteRenderer>().enabled = true;
+        crabOnes.SetActive(false);
+        crabTwos.SetActive(false);
+        pOneHealth.currentHealth = pOneHealth.maxHealth;
+        pTwoHealth.currentHealth = pTwoHealth.maxHealth;
+        crabOne.transform.position = playerOneStartPos;
+        crabTwo.transform.position = playerTwoStartPos;
+        guessPart.SetActive(false);
+    }
+
+    IEnumerator BlinkOne()
+    {
+        while (true)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                chara[i].sprite = charaImages[i * 2];
+            }
+            yield return new WaitForSeconds(0.35f);
+            for (int i = 0; i < 3; i++)
+            {
+                chara[i].sprite = charaImages[i * 2 + 1];
+            }
+            yield return new WaitForSeconds(0.35f);
+        }
+    }
+    IEnumerator BlinkTwo()
+    {
+        while (true)
+        {
+            for (int i = 3; i < 6; i++)
+            {
+                chara[i].sprite = charaImages[i * 2];
+            }
+            yield return new WaitForSeconds(0.35f);
+            for (int i = 3; i < 6; i++)
+            {
+                chara[i].sprite = charaImages[i * 2 + 1];
+            }
+            yield return new WaitForSeconds(0.35f);
+        }
     }
 }
